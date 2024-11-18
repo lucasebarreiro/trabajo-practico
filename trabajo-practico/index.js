bf4182d4476e33d78493d3a562d2e1342b20a99f
lucasebarreiro <lucasbarreiro1@outlook.com>
2024-11-18 00:05:36 -0300

    actividad-practica-2-terminada

--- trabajo-practico/index.js
+++ trabajo-practico/index.js
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+const axios = require('axios');
+
+const url = 'https://reclutamiento-dev-procontacto-default-rtdb.firebaseio.com/reclutier.json';
+
+    axios.get(url)
+    .then((response) => {
+        console.log('Datos recibidos:', response.data)
+    })
+    .catch((error)=>{
+        console.log('Error al obtener datos', error.message)
+    });
