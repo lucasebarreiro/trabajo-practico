8b3315eae2c1ce654c241ce6fd7b9124cb43a97d
lucasebarreiro <lucasbarreiro1@outlook.com>
2024-11-19 15:49:44 -0300

    terminando actividad practica 3

--- src/recruit/recruit.controller.ts
+++ src/recruit/recruit.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateRecruitDto } from './dto/create-recruit.dto';
+import { RecruitService } from './recruit.service';
 
 @Controller('recruit')
-export class RecruitController {}
+export class RecruitController {
+  constructor(private readonly recruitService: RecruitService) {}
+
+  @Post()
+  async createRecruit(
+    @Body(new ValidationPipe({ transform: true })) body: CreateRecruitDto,
+  ) {
+    body.name = this.capitalizeFirstLetter(body.name);
+    body.suraname = this.capitalizeFirstLetter(body.suraname);
+
+    const birthday = new Date(body.birthday);
+    const today = new Date();
+    if (birthday > today || birthday < new Date('1900/01/01')) {
+      throw new BadRequestException('Invalid birthday date');
+    }
+
+    const response = await this.recruitService.sendRecruitData(body);
+    return response;
+  }
+
+  private capitalizeFirstLetter(name: string): string {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  }
+}
--- src/recruit/recruit.module.ts
+++ src/recruit/recruit.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { RecruitController } from './recruit.controller';
 import { RecruitService } from './recruit.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RecruitController],
-  providers: [RecruitService]
+  providers: [RecruitService],
 })
 export class RecruitModule {}
